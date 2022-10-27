e8b1c61cea9de735b02bfa5e8e611524feb9b5e5
reid <reidbras@gmail.com>
2022-10-26 20:58:05 -0500

    Renaming App.js App.jsx

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css';
+import { Component} from 'react';
+import Header from './components/Header.jsx';
+import Footer from './components/Footer.jsx';
+
+
+class App extends Component {
+  render() {
+    return (
+      <main>
+        <Header className="App-header" Title="React Memory Game"></Header>
+        <Footer Text="React Memory Card Project 11/22"></Footer>
+      </main>);
+  }
+}
+export default App;
