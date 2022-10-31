55e903d62a336f515daa8c51a59c3a3c991d174e
reid <reidbras@gmail.com>
2022-10-31 15:23:29 -0500

    Removing class from container element -- moved to App.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -11,7 +11,7 @@ class Header extends Component {
         }
     }
     render() {
-        return (<div className="App-header">
+        return (<div className="">
             <h1 className="">{this.props.Title}</h1>
             <p className="">Objective: Answer the questions to test your memory.
                 <br></br>Highest score is 10!
