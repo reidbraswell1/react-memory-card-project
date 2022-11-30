48e65e3486a6267190cd95e56bd85980ee09a799
reid <reidbras@gmail.com>
2022-11-30 16:37:11 -0600

    Moving larg functions into their own utility file - processLanguageSelection and processSelection

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,19 @@ import Footer from './components/Footer.jsx';
 import { config } from './utils/config.js';
 import { toHaveClass } from '@testing-library/jest-dom/dist/matchers';
 import { useRef } from 'react';
+import { langEN } from './utils/lang.en.js';
+import { processLanguageSelection } from './utils/processLanguageSelection.js';
+import { processSelection } from './utils/processSelection.js';
 
 
 class App extends Component {
 
   constructor(props) {
     super(props);
-    this.processSelection = this.processSelection.bind(this);
+    this.processLanguageSelection = processLanguageSelection.bind(this);
+    this.processSelection = processSelection.bind(this);
     this.processChoice = this.processChoice.bind(this);
+
     this.state = {
       randomSymbols: [],
       displayedSymbols: [],
@@ -20,202 +25,17 @@ class App extends Component {
       userScore: 0,
       bestScore: 0,
       computerScore: 0,
+      heading: "",
+      heading: langEN.heading,
+      objective: langEN.objective,
+      highestScore: langEN.highestScore,
+      scorePrompt: langEN.score,
+      bestScorePrompt: langEN.bestScore,
+      computerScorePrompt: langEN.computerScore,
 
     }
   }
 
-  processSelection(event) {
-    console.log(`---Begin Function ${this.processSelection.name}---`);
-    console.log(`Event =`, event);
-    console.log(`Selected Options =`, event.target.selectedOptions);
-
-    let symbols = [];
-    if (event.target.selectedOptions.length > 1) {
-      console.log(`Error`);
-      return;
-    }
-    switch (event.target.selectedOptions[0].value) {
-      case "generalPunctuation1":
-        try {
-          for (let i = 0; i < config.maxSymbolSet; i++) {
-            symbols.push(Math.floor(Math.random() * (config.generalPunctuationMax1 - config.generalPunctuationMin1) + config.generalPunctuationMin1));
-          }
-          let data = this.state.displayedSymbols;
-          data.push(symbols[0]);
-          this.setState({ displayedSymbols: data })
-          document.getElementById("app-item-container").style.display="flex";
-          document.getElementById("span-symbol").innerHTML = `&#${symbols[0]};`;
-          document.getElementById("debug-symbol").innerHTML = `&#${symbols[0]};`;
-          this.setState({ randomSymbols: symbols.slice(0) });
-          this.setState({ currentSymbol: symbols[0] });
-          //document.getElementById("app-item-container").style.display="flex";
-        }
-        catch (err) {
-          console.log(`Error Has Occurred =`, err);
-        }
-        break;
-      case "generalPunctuation2":
-        try {
-          for (let i = 0; i < config.maxSymbolSet; i++) {
-            symbols.push(Math.floor(Math.random() * (config.generalPunctuationMax2 - config.generalPunctuationMin2) + config.generalPunctuationMin2));
-          }
-          document.getElementById("app-item-container").style.display="flex";
-          document.getElementById("span-symbol").innerHTML = `&#${symbols[0]};`;
-          document.getElementById("debug-symbol").innerHTML = `&#${symbols[0]};`;
-          this.setState({ randomSymbols: symbols.slice(0) });
-        }
-        catch (err) {
-          console.log(`Error Has Occurred =`, err);
-        }
-        break;
-      case "currencySymbols":
-        try {
-          for (let i = 0; i < config.maxSymbolSet; i++) {
-            symbols.push(Math.floor(Math.random() * (config.currencySymbolsMax - config.currencySymbolsMin) + config.currencySymbolsMin));
-          }
-          document.getElementById("app-item-container").style.display="flex";
-          document.getElementById("span-symbol").innerHTML = `&#${symbols[0]};`;
-          document.getElementById("debug-symbol").innerHTML = `&#${symbols[0]};`;
-          this.setState({ randomSymbols: symbols.slice(0) });
-        }
-        catch (err) {
-          console.log(`Error Has Occurred =`, err);
-        }
-        break;
-      case "arrows":
-        try {
-          for (let i = 0; i < config.maxSymbolSet; i++) {
-            symbols.push(Math.floor(Math.random() * (config.arrowsMax - config.arrowsMin) + config.arrowsMin));
-          }
-          document.getElementById("app-item-container").style.display="flex";
-          document.getElementById("span-symbol").innerHTML = `&#${symbols[0]};`;
-          document.getElementById("debug-symbol").innerHTML = `&#${symbols[0]};`;
-          this.setState({ randomSymbols: symbols.slice(0) });
-        }
-        catch (err) {
-          console.log(`Error Has Occurred =`, err);
-        }
-        break;
-      case "mathematical-operators":
-        try {
-          for (let i = 0; i < config.maxSymbolSet; i++) {
-            symbols.push(Math.floor(Math.random() * (config.mathSymbolsMax - config.mathSymbolsMin) + config.mathSymbolsMin));
-          }
-          document.getElementById("app-item-container").style.display="flex";
-          document.getElementById("span-symbol").innerHTML = `&#${symbols[0]};`;
-          document.getElementById("debug-symbol").innerHTML = `&#${symbols[0]};`;
-          this.setState({ randomSymbols: symbols.slice(0) });
-        }
-        catch (err) {
-          console.log(`Error Has Occurred =`, err);
-        }
-        break;
-      case "box-drawings":
-        try {
-          for (let i = 0; i < config.maxSymbolSet; i++) {
-            symbols.push(Math.floor(Math.random() * (config.boxDrawingsMax - config.boxDrawingsMin) + config.boxDrawingsMin));
-          }
-          document.getElementById("app-item-container").style.display="flex";
-          document.getElementById("span-symbol").innerHTML = `&#${symbols[0]};`;
-          document.getElementById("debug-symbol").innerHTML = `&#${symbols[0]};`;
-          this.setState({ randomSymbols: symbols.slice(0) });
-        }
-        catch (err) {
-          console.log(`Error Has Occurred =`, err);
-        }
-        break;
-      case "block-elements":
-        try {
-          for (let i = 0; i < config.maxSymbolSet; i++) {
-            symbols.push(Math.floor(Math.random() * (config.blockElementsMax - config.blockElementsMin) + config.blockElementsMin));
-          }
-          document.getElementById("app-item-container").style.display="flex";
-          document.getElementById("span-symbol").innerHTML = `&#${symbols[0]};`;
-          document.getElementById("debug-symbol").innerHTML = `&#${symbols[0]};`;
-          this.setState({ randomSymbols: symbols.slice(0) });
-        }
-        catch (err) {
-          console.log(`Error Has Occurred =`, err);
-        }
-        break;
-      case "miscelaneous-symbols":
-        try {
-          for (let i = 0; i < config.maxSymbolSet; i++) {
-            symbols.push(Math.floor(Math.random() * (config.miscelaneousSymbolsMax - config.miscelaneousSymbolsMin) + config.geometricShapesMin));
-          }
-          document.getElementById("app-item-container").style.display="flex";
-          document.getElementById("span-symbol").innerHTML = `&#${symbols[0]};`;
-          document.getElementById("debug-symbol").innerHTML = `&#${symbols[0]};`;
-          this.setState({ randomSymbols: symbols.slice(0) });
-        }
-        catch (err) {
-          console.log(`Error Has Occurred =`, err);
-        }
-        break;
-      case "geometric-shapes":
-        try {
-          for (let i = 0; i < config.maxSymbolSet; i++) {
-            symbols.push(Math.floor(Math.random() * (config.geometricShapesMax - config.geometricShapesMin) + config.geometricShapesMin));
-          }
-          document.getElementById("app-item-container").style.display="flex";
-          document.getElementById("span-symbol").innerHTML = `&#${symbols[0]};`;
-          document.getElementById("debug-symbol").innerHTML = `&#${symbols[0]};`;
-          this.setState({ randomSymbols: symbols.slice(0) });
-        }
-        catch (err) {
-          console.log(`Error Has Occurred =`, err);
-        }
-        break;
-      case "dingbats":
-        try {
-          for (let i = 0; i < config.maxSymbolSet; i++) {
-            symbols.push(Math.floor(Math.random() * (config.dingbatsMax - config.dingbatsMin) + config.dingbatsMin));
-          }
-          document.getElementById("app-item-container").style.display="flex";
-          document.getElementById("span-symbol").innerHTML = `&#${symbols[0]};`;
-          document.getElementById("debug-symbol").innerHTML = `&#${symbols[0]};`;
-          this.setState({ randomSymbols: symbols.slice(0) });
-        }
-        catch (err) {
-          console.log(`Error Has Occurred =`, err);
-        }
-        break;
-      case "emoji":
-        try {
-          let emojis = config.emoji.length;
-          for (let i = 0; i < config.maxSymbolSet; i++) {
-            let randomIdx = Math.floor(Math.random() * (config.emoji.length - 1) + 1);
-            symbols.push(config.emoji[randomIdx]);
-          }
-          document.getElementById("app-item-container").style.display="flex";
-          document.getElementById("span-symbol").innerHTML = `&#${symbols[0]};`;
-          document.getElementById("debug-symbol").innerHTML = `&#${symbols[0]};`;
-          this.setState({ randomSymbols: symbols.slice(0) });
-        }
-        catch (err) {
-          console.log(`Error Has Occurred =`, err);
-        }
-        break;
-      case "emoji-smileys":
-        try {
-          for (let i = 0; i < config.maxSymbolSet; i++) {
-            let randomIdx = Math.floor(Math.random() * (config.emojiSmileys.length - 1) + 1);
-            symbols.push(config.emojiSmileys[randomIdx]);
-          }
-          document.getElementById("app-item-container").style.display="flex";
-          document.getElementById("span-symbol").innerHTML = `&#${symbols[0]};`;
-          document.getElementById("debug-symbol").innerHTML = `&#${symbols[0]};`;
-          this.setState({ randomSymbols: symbols.slice(0) });
-        }
-        catch (err) {
-          console.log(`Error Has Occurred =`, err);
-        }
-        break;
-      default:
-        break;
-    }
-    console.log(`---End Function ${this.processSelection.name}---`);
-  }
 
   processChoice(event) {
     console.log(`---Begin ${this.processChoice.name}---`);
@@ -296,9 +116,9 @@ class App extends Component {
     return (
       <main className="App">
         <header className="App-header">
-          <Header Title="React Memory Game" score={this.state.userScore} bestScore={this.state.bestScore} computerScore={this.state.computerScore} highestScore={config.maxSymbolSet}></Header>
+          <Header heading={this.state.heading} objective={this.state.objective} highestScorePrompt={this.state.highestScore} scorePrompt={this.state.scorePrompt} score={this.state.userScore} bestScorePrompt={this.state.bestScorePrompt} bestScore={this.state.bestScore} computerScorePrompt={this.state.computerScorePrompt} computerScore={this.state.computerScore} highestScore={config.maxSymbolSet} processLanguageSelection={this.processLanguageSelection}></Header>
         </header>
-        <label className="labels" for="symbol-select">Select Symbol</label>
+        <label className="labels" htmlFor="symbol-select">Select Symbol</label>
         <select className="App-select pt-2 pb-2" id="symbol-select" name="symbol" onChange={this.processSelection}>
           <option value="select">Select an HTML symbol</option>
           <option value="generalPunctuation1">General Punctuation 1 {config.generalPunctuationMin1}-{config.generalPunctuationMax1}</option>
@@ -334,6 +154,8 @@ class App extends Component {
             <span>{`Random Symbols = ${this.state.randomSymbols.toString()}`}</span>
             <br></br>
             <span>User Score: {this.state.userScore} Best Score: {this.state.bestScore} Computer Score: {this.state.computerScore}</span>
+            <br></br>
+            <span>Heading: {config.heading}</span>
           </div>
         }
         <Footer Footer="React Memory Card Project 11/22"></Footer>
