7a94ac53fffca0df8eaf88d2717faa4cf9ff276f
reid <reidbras@gmail.com>
2022-11-30 16:21:24 -0600

    Changing from hard coded prompts to language configurable prompts

--- src/components/ScoreCard.jsx
+++ src/components/ScoreCard.jsx
@@ -6,9 +6,9 @@ class ScoreCard extends Component {
     }
     render() {
         return (<div className="App-scoreboard mt-4 mb-5 pt-2 pb-2">
-            <span>Score: {this.props.score}</span>
-            <span>Best Score: {this.props.bestScore}</span>
-            <span>Computer Score: {this.props.computerScore}</span>
+            <span>{this.props.scorePrompt} {this.props.score}</span>
+            <span>{this.props.bestScorePrompt} {this.props.bestScore}</span>
+            <span>{this.props.computerScorePrompt} {this.props.computerScore}</span>
             </div>);
     }
 }
