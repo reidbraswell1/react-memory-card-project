4c3c020195ccc87b74255979888a714b9d13db7e
reid <reidbras@gmail.com>
2022-11-30 14:49:53 -0600

    Adding language dictionaries

---
+++ src/utils/lang.cn.js
@@ -0,0 +1,9 @@
+const langCN = {
+    heading: "反应记忆游戏",
+    objective: "目的：回答问题来测试你的记忆力。",
+    highestScore: "最高分是 ",
+    score: "得分:",
+    bestScore: "最佳成绩:",
+    computerScore: "电脑评分:",
+}
+export { langCN };
---
+++ src/utils/lang.en.js
@@ -0,0 +1,9 @@
+const langEN = {
+    heading: "React Memory Game",
+    objective:"Objective: Answer the questions to test your memory.",
+    highestScore:"Highest score is ",
+    score:"Score:",
+    bestScore:"Best Score:",
+    computerScore:"Computer Score:",
+}
+export { langEN };
