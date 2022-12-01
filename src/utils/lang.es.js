b97060f573034e0ace5fa8d722cf3f830fd38e25
reid <reidbras@gmail.com>
2022-11-30 19:53:30 -0600

    Adding additional prompt translations to language dictionary

---
+++ src/utils/lang.es.js
@@ -0,0 +1,14 @@
+const langES = {
+    heading: "Reaccionar Memoria Juego",
+    objective: "Objetivo: Responder a las preguntas para poner a prueba tu memoria.",
+    highestScore: "La puntuación más alta es",
+    score: "Puntuación:",
+    bestScore: "Mejor puntuación:",
+    computerScore: "Puntuación de la computadora:",
+    selectSymbolLabelPrompt: "Seleccione un símbolo",
+    selectSymbolPrompt: "Seleccione un símbolo HTML",
+    questionPrompt: "¿Ya has visto este símbolo?",
+    yesPrompt: "Sí",
+    noPrompt: "No",
+}
+export { langES };
