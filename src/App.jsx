1cb881ace3a998d5fb282b541b414a9d73fcda27
reid <reidbras@gmail.com>
2022-12-02 13:33:39 -0600

    Moving function processChoice into its own file due to size.

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,7 @@ import { useRef } from 'react';
 import { langEN } from './utils/lang.en.js';
 import { processLanguageSelection } from './utils/processLanguageSelection.js';
 import { processSelection } from './utils/processSelection.js';
+import { processChoice } from './utils/processChoice.js';
 
 
 class App extends Component {
@@ -16,7 +17,7 @@ class App extends Component {
     super(props);
     this.processLanguageSelection = processLanguageSelection.bind(this);
     this.processSelection = processSelection.bind(this);
-    this.processChoice = this.processChoice.bind(this);
+    this.processChoice = processChoice.bind(this);
 
     this.state = {
       randomSymbols: [],
@@ -37,82 +38,6 @@ class App extends Component {
       questionPrompt: langEN.questionPrompt,
       yesPrompt: langEN.yesPrompt,
       noPrompt: langEN.noPrompt,
-    }
-  }
-
-
-  processChoice(event) {
-    console.log(`---Begin ${this.processChoice.name}---`);
-    console.log("event2", event);
-    const randomIndex = Math.floor(Math.random() * (config.maxSymbolSet - 1) + 1);
-    console.log("RandomIndex", randomIndex);
-    let userScore = this.state.userScore;
-    let computerScore = this.state.computerScore;
-    let bestScore = this.state.bestScore;
-    const currentSymbol = this.state.currentSymbol;
-    let displayedSymbolsNew = this.state.displayedSymbols;
-    const randomSymbols = this.state.randomSymbols;
-    // Randomly select the next symbol to display from the display pool
-    const newCurrentSymbol = randomSymbols[randomIndex - 1];
-    switch (event.target.value) {
-      case "yes":
-        {
-          document.getElementById("span-symbol").innerHTML = `&#${newCurrentSymbol};`;
-          document.getElementById("debug-symbol").innerHTML = `&#${newCurrentSymbol};`;
-          // Add the newly selected symbol to the list of displayed symbols
-          displayedSymbolsNew.push(newCurrentSymbol);
-          // Update the displayed symbols to include the newly selected symbol
-          this.setState({ displayedSymbols: displayedSymbolsNew });
-          const firstIndex = displayedSymbolsNew.sort().indexOf(currentSymbol);
-          const lastIndex = displayedSymbolsNew.sort().lastIndexOf(currentSymbol);
-          if (lastIndex > firstIndex) {
-            this.setState({ userScore: ++userScore });
-            if (userScore > bestScore) {
-              this.state.setState({ bestScore: userScore });
-            }
-            console.log(`Score user =`, userScore);
-          }
-          else {
-            this.setState({ computerScore: ++computerScore });
-            if (userScore > bestScore) {
-              this.state.setState({ bestScore: userScore });
-            }
-            this.setState({ userScore: 0 });
-            console.log(`Score computer =`, computerScore);
-          }
-          this.setState({ currentSymbol: newCurrentSymbol });
-        }
-        break;
-      case "no":
-        {
-          document.getElementById("span-symbol").innerHTML = `&#${newCurrentSymbol};`
-          document.getElementById("debug-symbol").innerHTML = `&#${newCurrentSymbol};`;
-          // Add the newly selected symbol to the list of displayed symbols
-          displayedSymbolsNew.push(newCurrentSymbol);
-          // Update the displayed symbols to include the newly selected symbol
-          this.setState({ displayedSymbols: displayedSymbolsNew });
-          const firstIndex = displayedSymbolsNew.sort().indexOf(currentSymbol);
-          const lastIndex = displayedSymbolsNew.sort().lastIndexOf(currentSymbol);
-          if (lastIndex > firstIndex) {
-            this.setState({ computerScore: ++computerScore });
-            if (userScore > bestScore) {
-              this.state.setState({ bestScore: userScore });
-            }
-            this.setState({ userScore: 0 });
-            console.log(`Score computer =`, computerScore);
-          }
-          else {
-            this.setState({ userScore: ++userScore });
-            console.log(`Score user =`, userScore);
-            if (userScore > bestScore) {
-              this.setState({ bestScore: userScore });
-            }
-          }
-          this.setState({ currentSymbol: newCurrentSymbol });
-        }
-        break;
-      default:
-        break;
     }
   }
 
