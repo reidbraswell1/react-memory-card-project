2fb8eeb15c5a9bb773007c6045c53b6916bb1978
reid <reidbras@gmail.com>
2022-12-08 16:05:12 -0600

    Adding French language file

---
+++ src/utils/lang.fr.js
@@ -0,0 +1,28 @@
+// French dictionary
+const langFR = {
+    heading: "Jeu de Mémoire React",
+    objective:"Objectif: Répondez aux questions pour tester votre mémoire.",
+    highestScore:"Le score le plus élevé est",
+    score:"Score:",
+    bestScore:"Meilleur Score:",
+    computerScore:"Score de l'ordinateur:",
+    selectSymbolLabelPrompt: "Sélectionnez le symbole",
+    selectSymbolPrompt: "Sélectionner un symbole HTML",
+    questionPrompt: "Avez-vous déjà vu ce symbole?",
+    yesPrompt: "Oui",
+    noPrompt: "Non",
+    generalPunctuation_1: "Ponctuation générale 1",
+    generalPunctuation_2: "Ponctuation générale 2",
+    currencySymbols: "Symboles monétaires",
+    arrows: "Flèches",
+    mathematicaloperators: "Opérateurs mathématiques",
+    boxDrawings: "Dessins de boîte",
+    blockElements: "Éléments de bloc",
+    geometricShapes: "Formes géométriques",
+    miscellaneousSymbols: "Symboles divers",
+    dingbats: "Dingbats",
+    emoji: "Emoji",
+    emojiSmileys: "Emoji Smileys",
+
+}
+export { langFR };
