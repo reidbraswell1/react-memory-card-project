a3ec87a745f249b8dc64205e4b114348263d6512
reid <reidbras@gmail.com>
2022-12-09 14:07:30 -0600

    Adding german language dictionary

---
+++ src/utils/lang.de.js
@@ -0,0 +1,28 @@
+// German dictionary
+const langDE = {
+    heading: "React Memory Spiel",
+    objective:"Ziel: Beantworten Sie die Fragen, um Ihr Gedächtnis zu testen.",
+    highestScore:"Höchste Punktzahl ist",
+    score:"Punktzahl:",
+    bestScore:"Beste Punktzahl:",
+    computerScore:"Computer-Score:",
+    selectSymbolLabelPrompt: "Symbol auswählen",
+    selectSymbolPrompt: "HTML-Symbol auswählen",
+    questionPrompt: "Haben Sie dieses Symbol schon gesehen?",
+    yesPrompt: "Ja",
+    noPrompt: "Nein",
+    generalPunctuation_1: "Allgemeine Interpunktion 1",
+    generalPunctuation_2: "Allgemeine Interpunktion 2",
+    currencySymbols: "Währungssymbole",
+    arrows: "Pfeile",
+    mathematicaloperators: "Mathematische Operatoren",
+    boxDrawings: "Box-Zeichnungen",
+    blockElements: "Blockelemente",
+    geometricShapes: "Geometrische Formen",
+    miscellaneousSymbols: "Verschiedene Symbole",
+    dingbats: "Dingbats",
+    emoji: "Emoji",
+    emojiSmileys: "Emoji Smileys",
+
+}
+export { langDE };
