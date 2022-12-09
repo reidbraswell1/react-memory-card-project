14a7151a0541e0cfdc522ee42a5c88e76e42fbcc
reid <reidbras@gmail.com>
2022-12-08 20:54:04 -0600

    Adding Italian language dictionary

---
+++ src/utils/lang.it.js
@@ -0,0 +1,28 @@
+// Italian dictionary
+const langIT = {
+    heading: "Gioco di memoria React",
+    objective:"Obiettivo: rispondere alle domande per testare la memoria.",
+    highestScore:"Il punteggio più alto è",
+    score:"Punteggio:",
+    bestScore:"Miglior punteggio:",
+    computerScore:"Punteggio del computer:",
+    selectSymbolLabelPrompt: "Seleziona simbolo",
+    selectSymbolPrompt: "Selezionare un simbolo HTML",
+    questionPrompt: "Hai già visto questo simbolo?",
+    yesPrompt: "Sì",
+    noPrompt: "No",
+    generalPunctuation_1: "Punteggiatura generale 1",
+    generalPunctuation_2: "Punteggiatura generale 2",
+    currencySymbols: "Simboli di valuta",
+    arrows: "Frecce",
+    mathematicaloperators: "Operatori matematici",
+    boxDrawings: "Disegni delle scatole",
+    blockElements: "Elementi a blocchi",
+    geometricShapes: "Forme geometriche",
+    miscellaneousSymbols: "Simboli vari",
+    dingbats: "Dingbats",
+    emoji: "Emoji",
+    emojiSmileys: "Emoji Smileys",
+
+}
+export { langIT };
