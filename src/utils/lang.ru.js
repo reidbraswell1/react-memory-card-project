9cfc65d22c01bbf810789abe489164d77efb2695
reid <reidbras@gmail.com>
2023-01-04 13:13:29 -0600

    Adding Russian language file

---
+++ src/utils/lang.ru.js
@@ -0,0 +1,28 @@
+// Russian dictionary
+const langRU = {
+    heading: "Игра с памятью React",
+    objective:"Цель: Ответьте на вопросы, чтобы проверить свою память.",
+    highestScore:"Наивысший балл",
+    score:"Счёт:",
+    bestScore:"Лучшая оценка:",
+    computerScore:"Компьютерная оценка:",
+    selectSymbolLabelPrompt: "Выберите символ",
+    selectSymbolPrompt: "Выбор символа HTML",
+    questionPrompt: "Вы уже видели этот символ?",
+    yesPrompt: "Да",
+    noPrompt: "Нет",
+    generalPunctuation_1: "Общая пунктуация 1",
+    generalPunctuation_2: "Общая пунктуация 2",
+    currencySymbols: "Символы валют",
+    arrows: "Стрелки",
+    mathematicaloperators: "Математические операторы",
+    boxDrawings: "Чертежи коробок",
+    blockElements: "Block Elements",
+    geometricShapes: "Геометрические фигуры",
+    miscellaneousSymbols: "Прочие символы",
+    dingbats: "Дингбаты",
+    emoji: "Эмодзи",
+    emojiSmileys: "Смайлики эмодзи",
+
+}
+export { langRU };
